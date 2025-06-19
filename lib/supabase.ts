873a0a2fcbbe70f0d7de6ba47eaca0e855542e6c
arjun13-ams <marketgainersnlosers@gmail.com>
2025-06-19 12:08:07 -0600

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,3 +1,3 @@
 import { createClient } from '@supabase/supabase-js';
 
-export const supabase = createClient('https://your-project.supabase.co', 'your-anon-key');
+export const supabase = createClient('https://ydszxdxrhbptiqxlywkr.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inlkc3p4ZHhyaGJwdGlxeGx5d2tyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg4MjAwNTAsImV4cCI6MjA2NDM5NjA1MH0.AtPKAW0vsNPFEMkKR9BIH-Q1GdwW2hzBR2dv2psZGQk');
